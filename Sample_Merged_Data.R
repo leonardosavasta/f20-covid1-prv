866c0e1c41d8082b628ac94927472287adebb03b
leonardosavasta <lsavasta90@gmail.com>
2020-10-11 01:49:00 -0400

    Organized data files into data folder

--- Sample_Merged_Data.R
+++ Sample_Merged_Data.R
@@ -4,14 +4,14 @@ library(tidyr)
 
 # Reading COVID-19 cases data by county for dates
 
-data1 <- read.csv("6-15-2020.csv",header = TRUE, sep= ",")
-data2 <- read.csv("7-19-2020.csv",header = TRUE, sep= ",")
+data1 <- read.csv("./data/6-15-2020.csv",header = TRUE, sep= ",")
+data2 <- read.csv("./data/7-19-2020.csv",header = TRUE, sep= ",")
 
 data <- merge(data1, data2, by= c("FIPS","Admin2","Province_State","Country_Region","Lat","Long_","Combined_Key"))
 
 # Reading mask usage data by county
 
-mask <- read.csv("Mask Usage.csv",header = TRUE, sep= ",")
+mask <- read.csv("./data/Mask Usage.csv",header = TRUE, sep= ",")
 
 # Merging cases info data with mask usage data by county
 
@@ -22,12 +22,12 @@ colnames(final_data)[2] = "County"
 
 # Reading data on GDP by county
 
-county_gdp <- read.csv("lagdp1219.csv", header = TRUE, sep= ",")
+county_gdp <- read.csv("./data/lagdp1219.csv", header = TRUE, sep= ",")
 
 # Data transformation
 
 # Get list of all states
-states <- read.csv("States.csv", header = TRUE, sep= ",")[,1]
+states <- read.csv("./data/States.csv", header = TRUE, sep= ",")[,1]
 
 # Create column of states for each county
 
@@ -42,5 +42,5 @@ colnames(county_gdp)[2] <- "GDP 2018"
 # Merging county GDP data with final data
 
 final_data <- merge(final_data, county_gdp, by= c("County","Province_State"))
-write.csv(final_data, "./final_data.csv")
+write.csv(final_data, "./data/final_data.csv")
 
