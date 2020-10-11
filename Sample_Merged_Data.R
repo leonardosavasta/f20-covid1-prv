62bbe0a74218785e03ea5219054677d867a9d67e
Rakshit-Pally26 <54860986+Rakshit-Pally26@users.noreply.github.com>
2020-10-11 14:17:52 -0400

    Update Sample_Merged_Data.R

--- Sample_Merged_Data.R
+++ Sample_Merged_Data.R
@@ -42,5 +42,17 @@ colnames(county_gdp)[2] <- "GDP 2018"
 # Merging county GDP data with final data
 
 final_data <- merge(final_data, county_gdp, by= c("County","Province_State"))
+
+# Reading data on Population_count by county
+pop_data1 <- read.csv("./data/population_data.csv",header = TRUE, sep= ",")
+
+# Data transformation
+pop_data1 <- separate(pop_data1, County_State, c("County", "State"), sep=",")
+pop_data1 <- pop_data1[, c(1,2,13)]
+colnames(pop_data1)[2] <- "Province_State"
+colnames(pop_data1)[3] <- "Population_Count_2019"
+
+# Merging county Population_Count data with final data
+final_data <- merge(final_data, pop_data1, by= c("County","Province_State"))
 write.csv(final_data, "./data/final_data.csv")
 
