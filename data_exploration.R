3a1d628229a979aa633e3f55bfe0ae91564da512
leonardosavasta <lsavasta90@gmail.com>
2020-10-16 22:32:39 -0400

    Updated graphs on data_exploration.R

--- data_exploration.R
+++ data_exploration.R
@@ -47,7 +47,9 @@ ggplot(
     x=Covid_Infection_Rate
   )
 ) +
-  geom_histogram(color="blue")
+  geom_histogram(aes(fill=..count..)) + 
+  ggtitle("Distribution of Covid Infection Rate per County") +
+  xlab("Covid Invection Rate") + ylab("Count")
 
 # Plot Median Household Income vs response
 
@@ -56,7 +58,9 @@ ggplot(
     clean_outliers(covid_data$Covid_Infection_Rate) 
     & clean_outliers(covid_data$Median_Household_Income),],
   aes(x=Median_Household_Income, y=Covid_Infection_Rate)) + 
-geom_point()
+geom_point() +
+  ggtitle("Socioeconomic Status vs Covid Infection Rate") +
+  xlab("Median Household Income") + ylab("Covid infection rate")
 
 # Plot Education vs response
 
@@ -66,7 +70,9 @@ ggplot(
   aes(
     x=associate_degree+bachelor_degree, 
     y=Covid_Infection_Rate)) + 
-geom_point()
+  geom_point() +
+  ggtitle("Education vs Covid Infection Rate") +
+  xlab("Ratio of Population With Associate Degree or Higher") + ylab("Covid infection rate")
 
 # Plot Age vs response
 
@@ -76,7 +82,9 @@ ggplot(
   aes(
     x=Median_Age,
     y=Covid_Infection_Rate)) + 
-  geom_point()
+  geom_point() +
+  ggtitle("Age vs Covid Infection Rate") +
+  xlab("County median age") + ylab("Covid infection rate")
 
 # Plot County Population vs response
 
@@ -85,7 +93,9 @@ ggplot(
     clean_outliers(covid_data$Covid_Infection_Rate) &
       clean_outliers(covid_data$Population_Count_2019),],
   aes(x=Population_Count_2019, y=Covid_Infection_Rate)) + 
-  geom_point()
+  geom_point() +
+  ggtitle("Population vs Covid Infection Rate") +
+  xlab("County population") + ylab("Covid infection rate")
 
 # Plot Usage of masks vs response
 
@@ -94,5 +104,7 @@ ggplot(
     clean_outliers(covid_data$Covid_Infection_Rate) &
       clean_outliers(covid_data$Population_Count_2019),],
   aes(x=FREQUENTLY + ALWAYS, y=Covid_Infection_Rate)) + 
-  geom_point()
+  geom_point() +
+  ggtitle("Mask Usage vs Covid Infection Rate") +
+  xlab("Ratio of population that always/freq. wears masks") + ylab("Covid infection rate")
 
