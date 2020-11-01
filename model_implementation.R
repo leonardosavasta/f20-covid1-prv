6679feecfb3697fb77993cdb8e34fcbd4c6b768d
Rakshit-Pally26 <54860986+Rakshit-Pally26@users.noreply.github.com>
2020-11-01 18:04:46 -0500

    Update model_implementation.R

--- model_implementation.R
+++ model_implementation.R
@@ -30,3 +30,45 @@ sampledata
 #predicting the response for the sampled data
 pred.data <- data.frame(sampledata)
 predict(lm.fit, pred.data)
+
+# fitting alternative(i.e. non-linear models)
+# fit Covid_Infection_Rate_Average with each individual predictors and produce plot of the data and smoothing line
+fit <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Household_Income, df=16)
+fit2 <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Household_Income, cv=TRUE)
+plot(data$Covid_Infection_Rate_Average, data$Median_Household_Income, cex=.5, col="darkgray")
+lines(fit,  col="red", lwd=2)
+lines(fit2, col="blue", lwd=2)
+legend("topright", legend=c("16 DF","6.8 DF (LOOCV)"), col=c("red", "blue"),lty=1, lwd=2, cex=.8)
+
+fit <- smooth.spline(data$Covid_Infection_Rate_Average, data$Population_Count_2019, df=16)
+fit2 <- smooth.spline(data$Covid_Infection_Rate_Average, data$Population_Count_2019, cv=TRUE)
+plot(data$Covid_Infection_Rate_Average, data$Population_Count_2019, cex=.5, col="darkgray")
+lines(fit,  col="red", lwd=2)
+lines(fit2, col="blue", lwd=2)
+legend("topright", legend=c("16 DF","6.8 DF (LOOCV)"), col=c("red", "blue"),lty=1, lwd=2, cex=.8)
+
+fit <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Age, df=16)
+fit2 <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Age, cv=TRUE)
+plot(data$Covid_Infection_Rate_Average, data$Median_Age, cex=.5, col="darkgray")
+lines(fit,  col="red", lwd=2)
+lines(fit2, col="blue", lwd=2)
+legend("topright", legend=c("16 DF","6.8 DF (LOOCV)"), col=c("red", "blue"),lty=1, lwd=2, cex=.8)
+
+
+gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Age), m=2), data=data)
+par(mfrow=c(3,2))
+par(mar=c(3,3,3,3))
+dev.new(width=5, height=4)
+plot(gam2, se=TRUE, col="blue")
+
+gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Household_Income), m=2), data=data)
+par(mfrow=c(3,2))
+par(mar=c(3,3,3,3))
+dev.new(width=5, height=4)
+plot(gam2, se=TRUE, col="blue")
+
+gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Household_Income), m=2), data=data)
+par(mfrow=c(3,2))
+par(mar=c(3,3,3,3))
+dev.new(width=5, height=4)
+plot(gam2, se=TRUE, col="blue")
