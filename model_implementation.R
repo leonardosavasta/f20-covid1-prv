830ad89404d369a362637b563a5788fbc5f1a4f4
apoo26 <37934161+apoo26@users.noreply.github.com>
2020-11-01 13:52:31 -0500

    Fit a multiple linear regression model and calculated test error rate

--- model_implementation.R
+++ model_implementation.R
@@ -13,20 +13,27 @@ data <- covid_data[, c("Population_Count_2019", "Median_Age", "Median_Household_
 data$MaskUsage <- covid_data$RARELY + covid_data$FREQUENTLY + covid_data$ALWAYS
 data$Education <- covid_data$associate_degree + covid_data$bachelor_degree
 
-lm.fit <- lm(Covid_Infection_Rate_Average ~ ., data=data)
+#fitting a multiple linear regression model
+library(boot)
+lm.fit <- glm(Covid_Infection_Rate_Average ~ ., data=data)
 summary(lm.fit)
 
+#performing 10 fold cross validation
+cv.error.10 <- cv.glm(data, lm.fit, K=10)
+cv.error.10$delta
+
+#fitting an alternative model
 fit2 <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Household_Income, cv=TRUE)
 
-gam2 <- gam(Covid_Infection_Rate_Average ~ 
-    s(log1p(Median_Age), m=2) + 
-    s(log1p(Median_Household_Income), m=1) + 
-    s(log1p(Education), m=3) + 
+gam2 <- gam(Covid_Infection_Rate_Average ~
+    s(log1p(Median_Age), m=2) +
+    s(log1p(Median_Household_Income), m=1) +
+    s(log1p(Education), m=3) +
     s(log1p(MaskUsage), m=2) +
-    s(log1p(Population_Count_2019), m=2), 
+    s(log1p(Population_Count_2019), m=2),
     data=data)
 
-gam2 <- gam(Covid_Infection_Rate_Average ~ 
+gam2 <- gam(Covid_Infection_Rate_Average ~
     s(log1p(Median_Age), m=2),
     data=data)
 
