610354f8449cdb698b340a7fa30e88e416e2ef25
leonardosavasta <lsavasta90@gmail.com>
2020-11-11 10:12:40 -0500

    Added file model_implementation.R for new model and loaded data

---
+++ model_implementation.R
@@ -0,0 +1,14 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+rm(list=ls())
+
+# Read data with predictors and response
+covid_data <- read.csv("./data/final_data.csv", header = TRUE, sep= ",")
+
+#dimensions of final data
+dim(covid_data)
+
+# Creating data frame with model predictors
+data <- covid_data[, c("Population_Count_2019", "Median_Age", "Median_Household_Income", "Covid_Infection_Rate_Average")]
+data$MaskUsage <- covid_data$RARELY + covid_data$FREQUENTLY + covid_data$ALWAYS
+data$Education <- covid_data$associate_degree + covid_data$bachelor_degree
+
