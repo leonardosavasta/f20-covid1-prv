e4564467ded2908627fa27e80ef303d1cf2df2eb
leonardosavasta <lsavasta90@gmail.com>
2020-11-18 12:19:39 -0500

    Updated out of sample predictions in linear_model_implementation.R based on feedback

--- linear_model_implementation.R
+++ linear_model_implementation.R
@@ -22,6 +22,12 @@ summary(lm.fit)
 cv.error.10 <- cv.glm(data, lm.fit, K=10)
 cv.error.10$delta
 
+
+
+
+# PREDICTIONS
+
+
 set.seed(2020)
 #sampling data to obtain 6 observations
 sampledata=data[sample(nrow(data), 6),]
@@ -30,6 +36,56 @@ sampledata
 #predicting the response for the sampled data
 pred.data <- data.frame(sampledata)
 predict(lm.fit, pred.data)
+
+
+# Constructing interesting predictions
+
+View(pred.data)
+
+# The following is similar to what we expect for a county such as Greenville
+pred.data <- data.frame(
+    Population_Count_2019= 500000,
+    Education= 0.30,
+    Median_Age= 40,
+    Median_Household_Income= 50000,
+    MaskUsage= 0.5
+)
+
+predict(lm.fit, pred.data)
+
+# We change the median household income increasing it by 30,000
+pred.data <- data.frame(
+    Population_Count_2019= 500000,
+    Education= 0.30,
+    Median_Age= 40,
+    Median_Household_Income= 80000,
+    MaskUsage= 0.5
+)
+
+predict(lm.fit, pred.data)
+
+# Our response variable (rate of infection) dramatically decreases
+# by modifying the median household income parameter
+
+# We change the Mask Usage from 0.5 to 0.9
+pred.data <- data.frame(
+    Population_Count_2019= 500000,
+    Education= 0.30,
+    Median_Age= 40,
+    Median_Household_Income= 50000,
+    MaskUsage= 0.9
+)
+
+predict(lm.fit, pred.data)
+
+# Our response variable (rate of infection) slightly decreases by modifying
+# the mask usage parameter
+
+
+
+
+
+
 
 # fitting alternative models(i.e. non-linear models)
 # fit Covid_Infection_Rate_Average with each individual predictors and produce plot of the data and smoothing line
