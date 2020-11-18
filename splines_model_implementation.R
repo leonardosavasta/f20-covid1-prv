a07fe8d08f4cb4dfe83249328863f1379bfa9349
ParthivaMondrety <69921934+ParthivaMondrety@users.noreply.github.com>
2020-11-18 18:33:15 -0500

    Update splines_model_implementation.R

--- splines_model_implementation.R
+++ splines_model_implementation.R
@@ -69,8 +69,32 @@ legend("topright", legend=c("16 DF","2.5 DF (LOOCV)"), col=c("red", "blue"),lty=
 gam.fit <- gam(Covid_Infection_Rate_Average ~ s(Median_Household_Income,4)+s(Population_Count_2019,5)+s(Median_Age,4)+s(MaskUsage,5)+s(Education,4))
 summary(gam.fit)
 
-# Performing Principal Component Analysis
+library(boot)
+#performing 10 fold cross validation
+cv.error.10 <- cv.glm(data, gam.fit, K=10)
+cv.error.10$delta
 
+par(mfrow= c(2,3))
+plot(gam.fit, se=TRUE, col="red")
+
+# Sampling training and testing data
+set.seed(1)
+N <- nrow(data)
+train <- sample(1:N, N/1.5)
+test <- seq(1:N)[-train]
+data.test <- data[test, "Covid_Infection_Rate_Average"]
+data.train <- data[train, "Covid_Infection_Rate_Average"]
+
+# Training smoothing splines model
+gam.fit <- gam(Covid_Infection_Rate_Average ~ s(Median_Household_Income,m=4) + s(Population_Count_2019,m=5) + s(Median_Age,m=4) + s(MaskUsage,m=5) + s(Education,m=4), data=data[train,], select=TRUE)
+yhat.gam <- predict(gam.fit, data[test,])
+mean((yhat.gam - data.test)^2)
+
+# Plotting true response vs predicted values
+ggplot(data[test,],aes(x=yhat.gam, y=Covid_Infection_Rate_Average)) +
+    geom_point(color="black")+geom_smooth(method='lm', color="red", size=0.5, alpha=0.8)
+
+# Performing Principal Component Analysis
 par(mfrow=c(1,1))
 prc.data <- prcomp(data, scale=TRUE)
 biplot(prc.data, scale=0, xlim=c(-5,5), ylim=c(-5,5), xlabs=rep(".", nrow(data)), col=c("red", "white"))
@@ -132,3 +156,22 @@ predict(gam.fit, pred.data)
 
 # Our response variable (rate of infection) slightly decreases by modifying
 # the mask usage parameter
+
+# ******* FITTING ALTERNATIVE MODELS *******
+gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Age), m=2), data=data)
+par(mfrow=c(3,2))
+par(mar=c(3,3,3,3))
+dev.new(width=5, height=4)
+plot(gam2, se=TRUE, col="blue")
+
+gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Household_Income), m=2), data=data)
+par(mfrow=c(3,2))
+par(mar=c(3,3,3,3))
+dev.new(width=5, height=4)
+plot(gam2, se=TRUE, col="blue")
+
+gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Household_Income), m=2), data=data)
+par(mfrow=c(3,2))
+par(mar=c(3,3,3,3))
+dev.new(width=5, height=4)
+plot(gam2, se=TRUE, col="blue")
