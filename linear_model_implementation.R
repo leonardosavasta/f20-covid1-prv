c4785a38f960a021c4aa4d9fe700e51d7c31c5ff
ParthivaMondrety <69921934+ParthivaMondrety@users.noreply.github.com>
2020-11-18 18:20:58 -0500

    Update linear_model_implementation.R

--- linear_model_implementation.R
+++ linear_model_implementation.R
@@ -1,6 +1,5 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 rm(list=ls())
-library(mgcv)
 
 # Read data with predictors and response
 covid_data <- read.csv("./data/final_data.csv", header = TRUE, sep= ",")
@@ -23,10 +22,7 @@ cv.error.10 <- cv.glm(data, lm.fit, K=10)
 cv.error.10$delta
 
 
-
-
 # ******* OUT OF SAMPLE PREDICTIONS *******
-
 
 set.seed(2020)
 #sampling data to obtain 6 observations
@@ -80,51 +76,3 @@ predict(lm.fit, pred.data)
 
 # Our response variable (rate of infection) slightly decreases by modifying
 # the mask usage parameter
-
-
-
-
-
-# ******* FITTING ALTERNATIVE MODELS *******
-
-# fitting alternative models(i.e. non-linear models)
-# fit Covid_Infection_Rate_Average with each individual predictors and produce plot of the data and smoothing line
-fit <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Household_Income, df=16)
-fit2 <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Household_Income, cv=TRUE)
-plot(data$Covid_Infection_Rate_Average, data$Median_Household_Income, cex=.5, col="darkgray")
-lines(fit,  col="red", lwd=2)
-lines(fit2, col="blue", lwd=2)
-legend("topright", legend=c("16 DF","6.8 DF (LOOCV)"), col=c("red", "blue"),lty=1, lwd=2, cex=.8)
-
-fit <- smooth.spline(data$Covid_Infection_Rate_Average, data$Population_Count_2019, df=16)
-fit2 <- smooth.spline(data$Covid_Infection_Rate_Average, data$Population_Count_2019, cv=TRUE)
-plot(data$Covid_Infection_Rate_Average, data$Population_Count_2019, cex=.5, col="darkgray")
-lines(fit,  col="red", lwd=2)
-lines(fit2, col="blue", lwd=2)
-legend("topright", legend=c("16 DF","6.8 DF (LOOCV)"), col=c("red", "blue"),lty=1, lwd=2, cex=.8)
-
-fit <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Age, df=16)
-fit2 <- smooth.spline(data$Covid_Infection_Rate_Average, data$Median_Age, cv=TRUE)
-plot(data$Covid_Infection_Rate_Average, data$Median_Age, cex=.5, col="darkgray")
-lines(fit,  col="red", lwd=2)
-lines(fit2, col="blue", lwd=2)
-legend("topright", legend=c("16 DF","6.8 DF (LOOCV)"), col=c("red", "blue"),lty=1, lwd=2, cex=.8)
-
-
-gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Age), m=2), data=data)
-par(mfrow=c(3,2))
-par(mar=c(3,3,3,3))
-dev.new(width=5, height=4)
-plot(gam2, se=TRUE, col="blue")
-
-gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Household_Income), m=2), data=data)
-par(mfrow=c(3,2))
-par(mar=c(3,3,3,3))
-dev.new(width=5, height=4)
-plot(gam2, se=TRUE, col="blue")
-
-gam2 <- gam(Covid_Infection_Rate_Average ~ s(log1p(Median_Household_Income), m=2), data=data)
-par(mfrow=c(3,2))
-par(mar=c(3,3,3,3))
-dev.new(width=5, height=4)
-plot(gam2, se=TRUE, col="blue")
